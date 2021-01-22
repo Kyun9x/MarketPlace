0a6c55b6f14ced7267d4653c0860803b979bf00e
Hải minh <hailmdev169@gmail.com>
2021-01-22 15:24:49 +0700

    Update MarketPlaceActivity.kt

--- MarketPlace/src/main/java/com/modul/marketplace/activity/marketplace/MarketPlaceActivity.kt
+++ MarketPlace/src/main/java/com/modul/marketplace/activity/marketplace/MarketPlaceActivity.kt
@@ -92,70 +92,70 @@ class MarketPlaceActivity : BaseActivity() {
             }
         }
     }
-
-    private fun apiMenuNvl(id: String) {
-        val callback: ApiRequest<NvlModelData> = ApiRequest()
-        callback.setCallBack(mApiSCM?.apiSCMProducts(1, mCartBussiness.getCartLocate().locateId, 1, 999),
-                { response -> onResponseServiceListNvl(response.data, id) }) { error ->
-            error.printStackTrace()
-        }
-    }
-
-    private fun onResponseServiceListNvl(data: ArrayList<NvlModel>?, id: String) {
-        data?.forEach {
-            if (it.id.equals(id)) {
-                val dmServiceListOrigin = DmServiceListOrigin()
-                dmServiceListOrigin.quantity = 0.0
-                dmServiceListOrigin.image = it.image_url_avatar
-                dmServiceListOrigin.imageUrls = it.image_urls
-                dmServiceListOrigin.name = it.name
-                dmServiceListOrigin.desc = it.description
-                dmServiceListOrigin.unitPrice = it.price!!
-                dmServiceListOrigin.marketPrice = it.price_sale!!
-                dmServiceListOrigin.productUid = it.id
-                dmServiceListOrigin.unitName = it.unit?.unit_name
-                dmServiceListOrigin.supplier_address = it.supplier?.supplier_address
-                dmServiceListOrigin.supplier_name = it.supplier?.supplier_name
-                dmServiceListOrigin.supplierUid = it.supplier_uid
-                dmServiceListOrigin.code = it.id
-                dmServiceListOrigin.brand_name = it.brand?.brand_name
-                dmServiceListOrigin.trademark = it.trademark
-
-                Handler().postDelayed({
-                    val bundle = Bundle()
-                    bundle.putSerializable(Constants.OBJECT, dmServiceListOrigin)
-                    openActivity(NvlDetailActivity::class.java, bundle = bundle)
-                }, 1000)
-
-            }
-        }
-    }
-
-    private fun apiMenuHermes(id: String) {
-        var productType = ""
-        if (mCartBussiness.appType == Constants.FABI) {
-            productType = Constants.FABI
-        } else {
-            productType = Constants.POSPC
-        }
-
-        val callback: ApiRequest<RestAllDmServiceListOrigin> = ApiRequest()
-        callback.setCallBack(mApiHermes?.apiOrderOnline_ServiceList(productType),
-                { response -> onResponseServiceList(response.data, id) }) { error ->
-        }
-    }
-
-    private fun onResponseServiceList(data: ArrayList<DmServiceListOrigin>?, id: String) {
-        data?.forEach {
-            if (it.uId.equals(id)) {
-                Handler().postDelayed({
-                    val bundle = Bundle()
-                    bundle.putSerializable(Constants.OBJECT, it)
-                    openActivity(PurchaseDetailActivity::class.java, bundle = bundle)
-                }, 1000)
-            }
-        }
-    }
+//
+//    private fun apiMenuNvl(id: String) {
+//        val callback: ApiRequest<NvlModelData> = ApiRequest()
+//        callback.setCallBack(mApiSCM?.apiSCMProducts(1, mCartBussiness.getCartLocate().locateId, 1, 999),
+//                { response -> onResponseServiceListNvl(response.data, id) }) { error ->
+//            error.printStackTrace()
+//        }
+//    }
+//
+//    private fun onResponseServiceListNvl(data: ArrayList<NvlModel>?, id: String) {
+//        data?.forEach {
+//            if (it.id.equals(id)) {
+//                val dmServiceListOrigin = DmServiceListOrigin()
+//                dmServiceListOrigin.quantity = 0.0
+//                dmServiceListOrigin.image = it.image_url_avatar
+//                dmServiceListOrigin.imageUrls = it.image_urls
+//                dmServiceListOrigin.name = it.name
+//                dmServiceListOrigin.desc = it.description
+//                dmServiceListOrigin.unitPrice = it.price!!
+//                dmServiceListOrigin.marketPrice = it.price_sale!!
+//                dmServiceListOrigin.productUid = it.id
+//                dmServiceListOrigin.unitName = it.unit?.unit_name
+//                dmServiceListOrigin.supplier_address = it.supplier?.supplier_address
+//                dmServiceListOrigin.supplier_name = it.supplier?.supplier_name
+//                dmServiceListOrigin.supplierUid = it.supplier_uid
+//                dmServiceListOrigin.code = it.id
+//                dmServiceListOrigin.brand_name = it.brand?.brand_name
+//                dmServiceListOrigin.trademark = it.trademark
+//
+//                Handler().postDelayed({
+//                    val bundle = Bundle()
+//                    bundle.putSerializable(Constants.OBJECT, dmServiceListOrigin)
+//                    openActivity(NvlDetailActivity::class.java, bundle = bundle)
+//                }, 1000)
+//
+//            }
+//        }
+//    }
+//
+//    private fun apiMenuHermes(id: String) {
+//        var productType = ""
+//        if (mCartBussiness.appType == Constants.FABI) {
+//            productType = Constants.FABI
+//        } else {
+//            productType = Constants.POSPC
+//        }
+//
+//        val callback: ApiRequest<RestAllDmServiceListOrigin> = ApiRequest()
+//        callback.setCallBack(mApiHermes?.apiOrderOnline_ServiceList(productType),
+//                { response -> onResponseServiceList(response.data, id) }) { error ->
+//        }
+//    }
+//
+//    private fun onResponseServiceList(data: ArrayList<DmServiceListOrigin>?, id: String) {
+//        data?.forEach {
+//            if (it.uId.equals(id)) {
+//                Handler().postDelayed({
+//                    val bundle = Bundle()
+//                    bundle.putSerializable(Constants.OBJECT, it)
+//                    openActivity(PurchaseDetailActivity::class.java, bundle = bundle)
+//                }, 1000)
+//            }
+//        }
+//    }
 
     private fun initMenu(pushNotify : NotificationModel?) {
         let {
